fbb5a4c5c3f29f43e56e16b00894ddbdab18e5d8
srav001 <sravansuresh1x@gmail.com>
2022-01-23 02:42:44 +0530

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        '@vue/airbnb',
+        'prettier'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'warn',
+        'no-debugger': 'off',
+        'vue/no-deprecated-slot-attribute': 'off',
+        "import/extensions": [ 2, {
+            'js': "always",
+            'vue': "always",
+            'ts': "never",
+          }],
+    },
+    parserOptions: {
+        parser: "@typescript-eslint/parser"
+    },
+};
